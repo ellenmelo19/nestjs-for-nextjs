9a67e1bb39a01dc93da28e2fac527ba2061f967a
ellenmelo <ellen.melo156@gmail.com>
2026-03-06 16:33:36 -0300

    Criando findAllOwned e findOneOwned Post para ver os posts do usuário logado

--- src/common/utils/create-slug-from-text.ts
+++ src/common/utils/create-slug-from-text.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { generateRandomSuffix } from "./generate-random-suffix";
-import { slugify } from './slugify';
-
+import { slugify } from "./slugify";
 export function createSlugFromText(text: string) {
   const slug = slugify(text);
   return `${slug}-${generateRandomSuffix()}`;
