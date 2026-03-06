a4e44788996645109b00048c1b8429e92eaa3466
ellenmelo <ellen.melo156@gmail.com>
2026-03-06 15:04:43 -0300

    Atualizando o usuário (PATCH) + usando mapped-types nos DTOs do NestJS

--- src/user/dto/update.user.dto.ts
+++ src/user/dto/update.user.dto.ts
@@ -1,1 +1,7 @@
-export class UpdateUserDto {}
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create.user.dto';
+
+export class UpdateUserDto extends PartialType(
+  OmitType(CreateUserDto, ['password']),
+) {}
