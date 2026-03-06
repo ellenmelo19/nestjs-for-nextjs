573853f5bb7ac4b30f62edd872cd25ec87ddbc2e
ellenmelo <ellen.melo156@gmail.com>
2026-03-06 14:35:16 -0300

    Assinando o JWT Token e autenticando o usuário com @nestjs/jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,33 @@
-import { Module } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { InternalServerErrorException, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { CommonModule } from 'src/common/pipes/common.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    CommonModule,
+    JwtModule.registerAsync({
+      useFactory: () => {
+        const secret = process.env.JWT_SECRET;
+
+        if (!secret) {
+          throw new InternalServerErrorException(
+            'JWT_SECRET not found in .env',
+          );
+        }
+
+        return {
+          secret,
+          signOptions: { expiresIn: process.env.JWT_EXPIRATION || '1d' },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [],
