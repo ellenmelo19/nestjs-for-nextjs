c015e3c4c49a37839f181aec17934fceb07ccf25
ellenmelo <ellen.melo156@gmail.com>
2026-03-06 16:40:39 -0300

    Criando o update Post para atualizar os dados do Post

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
   BadRequestException,
   Injectable,
@@ -10,6 +11,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { User } from 'src/user/entities/user.entity';
 import { createSlugFromText } from 'src/common/utils/create-slug-from-text';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostService {
@@ -96,5 +98,21 @@ export class PostService {
       });
 
     return created;
+  }
+
+  async update(postData: Partial<Post>, dto: UpdatePostDto, author: User) {
+    if (Object.keys(dto).length === 0) {
+      throw new BadRequestException('Dados não enviados');
+    }
+
+    const post = await this.findOneOwnedOrFail(postData, author);
+
+    post.title = dto.title ?? post.title;
+    post.content = dto.content ?? post.content;
+    post.excerpt = dto.excerpt ?? post.excerpt;
+    post.coverImageUrl = dto.coverImageUrl ?? post.coverImageUrl;
+    post.published = dto.published ?? post.published;
+
+    return this.postRepository.save(post);
   }
 }
